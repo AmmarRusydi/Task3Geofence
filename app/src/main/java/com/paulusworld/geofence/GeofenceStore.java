52e9928ce55d05dad387e54405c4e50aefac3096
kadi <ibnshafie@gmail.com>
2016-02-03 12:03:42 +0800

    Task3-geofence

---
+++ app/src/main/java/com/paulusworld/geofence/GeofenceStore.java
@@ -0,0 +1,682 @@
+package com.paulusworld.geofence;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.view.Display;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class GeofenceStore extends Context implements ConnectionCallbacks,
+        OnConnectionFailedListener, ResultCallback<Status>, LocationListener {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    /**
+     * Context
+     */
+    private Context mContext;
+
+    /**
+     * Google API client object.
+     */
+    private GoogleApiClient mGoogleApiClient;
+
+    /**
+     * Geofencing PendingIntent
+     */
+    private PendingIntent mPendingIntent;
+
+    /**
+     * List of geofences to monitor.
+     */
+    private ArrayList<Geofence> mGeofences;
+
+    /**
+     * Geofence request.
+     */
+    private GeofencingRequest mGeofencingRequest;
+
+    /**
+     * Location Request object.
+     */
+    private LocationRequest mLocationRequest;
+
+    /**
+     * Constructs a new GeofenceStore. 
+     *
+     * @param context The context to use.
+     * @param geofences List of geofences to monitor.
+     */
+    public GeofenceStore(Context context, ArrayList<Geofence> geofences) {
+        mContext = context;
+        mGeofences = new ArrayList<Geofence>(geofences);
+        mPendingIntent = null;
+
+        // Build a new GoogleApiClient, specify that we want to use LocationServices
+        // by adding the API to the client, specify the connection callbacks are in 
+        // this class as well as the OnConnectionFailed method. 
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).build();
+
+        // This is purely optional and has nothing to do with geofencing. 
+        // I added this as a way of debugging.
+        // Define the LocationRequest.
+        mLocationRequest = new LocationRequest();
+        // We want a location update every 30 seconds. ( 10000 = 10 seconds)
+        mLocationRequest.setInterval(30000);
+        // We want the location to be as accurate as possible.
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onResult(Status result) {
+        if (result.isSuccess()) {
+            Log.v(TAG, "Success!");
+        } else if (result.hasResolution()) {
+            // TODO Handle resolution
+        } else if (result.isCanceled()) {
+            Log.v(TAG, "Canceled");
+        } else if (result.isInterrupted()) {
+            Log.v(TAG, "Interrupted");
+        } else {
+
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.v(TAG, "Connection failed.");
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // We're connected, now we need to create a GeofencingRequest with
+        // the geofences we have stored.
+        mGeofencingRequest = new GeofencingRequest.Builder().addGeofences(
+                mGeofences).build();
+
+        mPendingIntent = createRequestPendingIntent();
+
+        // This is for debugging only and does not affect
+        // geofencing.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+//        if (locationManager != null) {
+//            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+//                    || checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//                locationManager.removeUpdates(GPSListener.this);
+//            }
+//        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+
+        // Submitting the request to monitor geofences.
+        PendingResult<Status> pendingResult = LocationServices.GeofencingApi
+                .addGeofences(mGoogleApiClient, mGeofencingRequest,
+                        mPendingIntent);
+
+        // Set the result callbacks listener to this class.
+        pendingResult.setResultCallback(this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.v(TAG, "Connection suspended.");
+    }
+
+    /**
+     * This creates a PendingIntent that is to be fired when geofence transitions
+     * take place. In this instance, we are using an IntentService to handle the
+     * transitions.
+     *
+     * @return A PendingIntent that will handle geofence transitions.
+     */
+    private PendingIntent createRequestPendingIntent() {
+        if (mPendingIntent == null) {
+            Log.v(TAG, "Creating PendingIntent");
+            Intent intent = new Intent(mContext, GeofenceIntentService.class);
+            mPendingIntent = PendingIntent.getService(mContext, 0, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+        }
+
+        return mPendingIntent;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        Log.v(TAG, "Location Information\n"
+                + "==========\n"
+                + "Provider:\t" + location.getProvider() + "\n"
+                + "Lat & Long:\t" + location.getLatitude() + ", "
+                + location.getLongitude() + "\n"
+                + "Altitude:\t" + location.getAltitude() + "\n"
+                + "Bearing:\t" + location.getBearing() + "\n"
+                + "Speed:\t\t" + location.getSpeed() + "\n"
+                + "Accuracy:\t" + location.getAccuracy() + "\n");
+    }
+
+    @Override
+    public AssetManager getAssets() {
+        return null;
+    }
+
+    @Override
+    public Resources getResources() {
+        return null;
+    }
+
+    @Override
+    public PackageManager getPackageManager() {
+        return null;
+    }
+
+    @Override
+    public ContentResolver getContentResolver() {
+        return null;
+    }
+
+    @Override
+    public Looper getMainLooper() {
+        return null;
+    }
+
+    @Override
+    public Context getApplicationContext() {
+        return null;
+    }
+
+    @Override
+    public void setTheme(int resid) {
+
+    }
+
+    @Override
+    public Resources.Theme getTheme() {
+        return null;
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        return null;
+    }
+
+    @Override
+    public String getPackageName() {
+        return null;
+    }
+
+    @Override
+    public ApplicationInfo getApplicationInfo() {
+        return null;
+    }
+
+    @Override
+    public String getPackageResourcePath() {
+        return null;
+    }
+
+    @Override
+    public String getPackageCodePath() {
+        return null;
+    }
+
+    @Override
+    public SharedPreferences getSharedPreferences(String name, int mode) {
+        return null;
+    }
+
+    @Override
+    public FileInputStream openFileInput(String name) throws FileNotFoundException {
+        return null;
+    }
+
+    @Override
+    public FileOutputStream openFileOutput(String name, int mode) throws FileNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean deleteFile(String name) {
+        return false;
+    }
+
+    @Override
+    public File getFileStreamPath(String name) {
+        return null;
+    }
+
+    @Override
+    public File getFilesDir() {
+        return null;
+    }
+
+    @Override
+    public File getNoBackupFilesDir() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public File getExternalFilesDir(String type) {
+        return null;
+    }
+
+    @Override
+    public File[] getExternalFilesDirs(String type) {
+        return new File[0];
+    }
+
+    @Override
+    public File getObbDir() {
+        return null;
+    }
+
+    @Override
+    public File[] getObbDirs() {
+        return new File[0];
+    }
+
+    @Override
+    public File getCacheDir() {
+        return null;
+    }
+
+    @Override
+    public File getCodeCacheDir() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public File getExternalCacheDir() {
+        return null;
+    }
+
+    @Override
+    public File[] getExternalCacheDirs() {
+        return new File[0];
+    }
+
+    @Override
+    public File[] getExternalMediaDirs() {
+        return new File[0];
+    }
+
+    @Override
+    public String[] fileList() {
+        return new String[0];
+    }
+
+    @Override
+    public File getDir(String name, int mode) {
+        return null;
+    }
+
+    @Override
+    public SQLiteDatabase openOrCreateDatabase(String name, int mode, SQLiteDatabase.CursorFactory factory) {
+        return null;
+    }
+
+    @Override
+    public SQLiteDatabase openOrCreateDatabase(String name, int mode, SQLiteDatabase.CursorFactory factory, DatabaseErrorHandler errorHandler) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteDatabase(String name) {
+        return false;
+    }
+
+    @Override
+    public File getDatabasePath(String name) {
+        return null;
+    }
+
+    @Override
+    public String[] databaseList() {
+        return new String[0];
+    }
+
+    @Override
+    public Drawable getWallpaper() {
+        return null;
+    }
+
+    @Override
+    public Drawable peekWallpaper() {
+        return null;
+    }
+
+    @Override
+    public int getWallpaperDesiredMinimumWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getWallpaperDesiredMinimumHeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWallpaper(Bitmap bitmap) throws IOException {
+
+    }
+
+    @Override
+    public void setWallpaper(InputStream data) throws IOException {
+
+    }
+
+    @Override
+    public void clearWallpaper() throws IOException {
+
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+
+    }
+
+    @Override
+    public void startActivity(Intent intent, Bundle options) {
+
+    }
+
+    @Override
+    public void startActivities(Intent[] intents) {
+
+    }
+
+    @Override
+    public void startActivities(Intent[] intents, Bundle options) {
+
+    }
+
+    @Override
+    public void startIntentSender(IntentSender intent, Intent fillInIntent, int flagsMask, int flagsValues, int extraFlags) throws IntentSender.SendIntentException {
+
+    }
+
+    @Override
+    public void startIntentSender(IntentSender intent, Intent fillInIntent, int flagsMask, int flagsValues, int extraFlags, Bundle options) throws IntentSender.SendIntentException {
+
+    }
+
+    @Override
+    public void sendBroadcast(Intent intent) {
+
+    }
+
+    @Override
+    public void sendBroadcast(Intent intent, String receiverPermission) {
+
+    }
+
+    @Override
+    public void sendOrderedBroadcast(Intent intent, String receiverPermission) {
+
+    }
+
+    @Override
+    public void sendOrderedBroadcast(Intent intent, String receiverPermission, BroadcastReceiver resultReceiver, Handler scheduler, int initialCode, String initialData, Bundle initialExtras) {
+
+    }
+
+    @Override
+    public void sendBroadcastAsUser(Intent intent, UserHandle user) {
+
+    }
+
+    @Override
+    public void sendBroadcastAsUser(Intent intent, UserHandle user, String receiverPermission) {
+
+    }
+
+    @Override
+    public void sendOrderedBroadcastAsUser(Intent intent, UserHandle user, String receiverPermission, BroadcastReceiver resultReceiver, Handler scheduler, int initialCode, String initialData, Bundle initialExtras) {
+
+    }
+
+    @Override
+    public void sendStickyBroadcast(Intent intent) {
+
+    }
+
+    @Override
+    public void sendStickyOrderedBroadcast(Intent intent, BroadcastReceiver resultReceiver, Handler scheduler, int initialCode, String initialData, Bundle initialExtras) {
+
+    }
+
+    @Override
+    public void removeStickyBroadcast(Intent intent) {
+
+    }
+
+    @Override
+    public void sendStickyBroadcastAsUser(Intent intent, UserHandle user) {
+
+    }
+
+    @Override
+    public void sendStickyOrderedBroadcastAsUser(Intent intent, UserHandle user, BroadcastReceiver resultReceiver, Handler scheduler, int initialCode, String initialData, Bundle initialExtras) {
+
+    }
+
+    @Override
+    public void removeStickyBroadcastAsUser(Intent intent, UserHandle user) {
+
+    }
+
+    @Nullable
+    @Override
+    public Intent registerReceiver(BroadcastReceiver receiver, IntentFilter filter) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Intent registerReceiver(BroadcastReceiver receiver, IntentFilter filter, String broadcastPermission, Handler scheduler) {
+        return null;
+    }
+
+    @Override
+    public void unregisterReceiver(BroadcastReceiver receiver) {
+
+    }
+
+    @Nullable
+    @Override
+    public ComponentName startService(Intent service) {
+        return null;
+    }
+
+    @Override
+    public boolean stopService(Intent service) {
+        return false;
+    }
+
+    @Override
+    public boolean bindService(Intent service, ServiceConnection conn, int flags) {
+        return false;
+    }
+
+    @Override
+    public void unbindService(ServiceConnection conn) {
+
+    }
+
+    @Override
+    public boolean startInstrumentation(ComponentName className, String profileFile, Bundle arguments) {
+        return false;
+    }
+
+    @Override
+    public Object getSystemService(String name) {
+        return null;
+    }
+
+    @Override
+    public String getSystemServiceName(Class<?> serviceClass) {
+        return null;
+    }
+
+    @Override
+    public int checkPermission(String permission, int pid, int uid) {
+        return 0;
+    }
+
+    @Override
+    public int checkCallingPermission(String permission) {
+        return 0;
+    }
+
+    @Override
+    public int checkCallingOrSelfPermission(String permission) {
+        return 0;
+    }
+
+    @Override
+    public int checkSelfPermission(String permission) {
+        return 0;
+    }
+
+    @Override
+    public void enforcePermission(String permission, int pid, int uid, String message) {
+
+    }
+
+    @Override
+    public void enforceCallingPermission(String permission, String message) {
+
+    }
+
+    @Override
+    public void enforceCallingOrSelfPermission(String permission, String message) {
+
+    }
+
+    @Override
+    public void grantUriPermission(String toPackage, Uri uri, int modeFlags) {
+
+    }
+
+    @Override
+    public void revokeUriPermission(Uri uri, int modeFlags) {
+
+    }
+
+    @Override
+    public int checkUriPermission(Uri uri, int pid, int uid, int modeFlags) {
+        return 0;
+    }
+
+    @Override
+    public int checkCallingUriPermission(Uri uri, int modeFlags) {
+        return 0;
+    }
+
+    @Override
+    public int checkCallingOrSelfUriPermission(Uri uri, int modeFlags) {
+        return 0;
+    }
+
+    @Override
+    public int checkUriPermission(Uri uri, String readPermission, String writePermission, int pid, int uid, int modeFlags) {
+        return 0;
+    }
+
+    @Override
+    public void enforceUriPermission(Uri uri, int pid, int uid, int modeFlags, String message) {
+
+    }
+
+    @Override
+    public void enforceCallingUriPermission(Uri uri, int modeFlags, String message) {
+
+    }
+
+    @Override
+    public void enforceCallingOrSelfUriPermission(Uri uri, int modeFlags, String message) {
+
+    }
+
+    @Override
+    public void enforceUriPermission(Uri uri, String readPermission, String writePermission, int pid, int uid, int modeFlags, String message) {
+
+    }
+
+    @Override
+    public Context createPackageContext(String packageName, int flags) throws PackageManager.NameNotFoundException {
+        return null;
+    }
+
+    @Override
+    public Context createConfigurationContext(Configuration overrideConfiguration) {
+        return null;
+    }
+
+    @Override
+    public Context createDisplayContext(Display display) {
+        return null;
+    }
+}
